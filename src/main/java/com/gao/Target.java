a8091d13fdbc08fef03a2d9d5426c1994a06f4b2
grxupupup <grxupupup@foxmail.com>
2021-01-02 12:21:53 +0800

    Initial commit

---
+++ src/main/java/com/gao/Target.java
@@ -0,0 +1,9 @@
+package com.gao;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Target {
+    public static void main(String[] args) {
+        System.out.println("github 很好用");
+    }
+}
