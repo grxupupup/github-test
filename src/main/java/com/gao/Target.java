579b7631d3834e97325d63dc16b2d9951ccca1f2
grxupupup <71170941+grxupupup@users.noreply.github.com>
2021-01-02 12:30:06 +0800

    Update Target.java

--- src/main/java/com/gao/Target.java
+++ src/main/java/com/gao/Target.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Target {
     public static void main(String[] args) {
         System.out.println("github 很好用");
+        System.out.println("在github上编辑，在IDEA上更新");
     }
 }
